f62d4fda6b7a72bd939669046891cd013ed05e26
Sho Kodama <sho.kodama@rakus.co.jp>
2015-09-07 12:03:37 +0900

    windowsからcommit

---
+++ src/main/java/com/example/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	
+		/**ユーザID*/
+		private Long id;
+		/**ユーザ名*/
+		private String name;
+		/**ユーザメールアドレス*/
+		private String email;
+		/**ユーザパスワード*/
+		private String password;
+		/**ユーザアドレス*/
+		private String address;
+		/**ユーザ電話番号*/
+		private String telephone;
+}
+
+

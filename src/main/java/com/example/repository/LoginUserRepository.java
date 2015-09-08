a55528b93a6dfb972d1e475c8868dc09566f0a6a
shokodama <madakodama.1go@gmail.com>
2015-09-08 12:00:56 +0900

    loginUserRepository実装。SQLにスキーマ流し込み。

--- src/main/java/com/example/repository/LoginUserRepository.java
+++ src/main/java/com/example/repository/LoginUserRepository.java
@@ -1,6 +1,13 @@
 package com.example.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import com.example.domain.User;
 
 /**
  * ユーザーログイン用のレポジトリ
@@ -9,10 +16,30 @@ import org.springframework.jdbc.core.RowMapper;
  */
 public class LoginUserRepository {
 	
-//	private static final RowMapper<User>
+	private static final RowMapper<User> USER_ROW_MAPPER = (rs, i) -> {
+		Long id = rs.getLong("id");
+		String name = rs.getString("name");
+		String email = rs.getString("email");
+		String password = rs.getString("password");
+		String address = rs.getString("address");
+		String telephone = rs.getString("telephone");
+		
+		return new User(id, name, email, password, address, telephone);
+	};
 	
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
 	
-	
-	
-	
+	public User findByMailAddress(String email){
+		User user = null;
+		try{
+			SqlParameterSource param = new MapSqlParameterSource().addValue("email", email);
+			user = jdbcTemplate.queryForObject("select id, name,email, password, address, telephone from users where email = :email",
+					param,
+					USER_ROW_MAPPER);
+			return user;	
+		}catch(DataAccessException e){
+			return null;
+		}
+	}
 }
